068a60ac69507c273d7172db25cb0e0cb9d418d6
ok <karinadaianova45@gmail.com>
2023-04-15 13:26:44 +0500

    123

--- defs_vk/chat.py
+++ defs_vk/chat.py
@@ -1,9 +1,20 @@
+import sqlite3
+
 from vkbottle.bot import Blueprint, Message
+from data.defs_orm import create_user, update_user, get_user
 
 bp = Blueprint("For chat commands")
 bp.on.vbml_ignore_case = True
+
 
 @bp.on.chat_message(text='Привет')
 async def start(msg: Message):
     await msg.answer('Ку')
 
+
+@bp.on.chat_message(text=['Передать <url> <money:int>', 'Передать <url>', 'Передать'])
+async def other(msg: Message, url: str = None, money: int = 0):
+    id = url[3:url.index('|')]
+    print(msg.peer_id)
+    money_user = (await get_user(id)).money
+    print(money_user)
